31cc7e26055247ea811752648fe6a4435a6a8ab2
= <victor.j.l.bernard@gmail.com>
2022-12-01 20:02:15 +0100

    additions & changes

---
+++ classes/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		UI ui = null;
+		try {
+		ui = UI.getNewUI(20, 20);
+		} catch (InstantiationException e) {
+			System.out.println("déjà instantié ???");
+		}
+		for (int i = 0; i < 20; i++)
+			for (int j = 0; j < 20; j++)
+				ui.setCell(i, j, '*', (int) i/(20/7) + 30, (int) j/(20/7) + 40);
+		ui.clearScreen();
+		ui.printBuffer();
+	}
+}
---
+++ classes/RessourceStatique.java
@@ -0,0 +1,1 @@
+public class RessourceStatique extends Ressource {}

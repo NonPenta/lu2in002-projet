d28b8e31ff05af1283d29475d5a8ed99b7747c2f
faotix <maximilien.piron-palliser.pro@orange.fr>
2022-12-04 17:14:56 +0100

    Amélioration du vent

--- classes/Vent.java
+++ classes/Vent.java
@@ -1,13 +1,17 @@
 public class Vent extends Agent implements ForceEnvironnementale {
-    public Vent(){}
+    private Vent(){}
 
     public void souffler(Consommable s, Terrain t){
         if (s.type != "Spore"){
             return;
         }
-        int vitesse = (int)(Math.random()*(13-1+1)+1);
-        int newX = (int)(Math.random()*(vitesse-0+1)+0);
-        int newY = (int)(Math.random()*((vitesse-newX)-0+1)+0);
-        t.setCase(newX,newY,s);
+        int vitesse = (int)(Math.random()*(13-1+1)+1);         // Initialisation vitesse du vent entre 130kmh et 10 kmh divisé par 10
+        int newX = s.getX() + (int)(Math.random()*(vitesse-0+1)+0);       // Création du nouveau x entre 0 et la vitesse
+        int newY = s.getY() + (int)(Math.random()*((vitesse-newX)-0+1)+0);// Création du nouveau y entre 0 et newX -> Afin que le déplacement ne soit pas seul un seul axe
+        while(!(newX<=t.nbLignes-1) && !(newY <=t.nbColonnes-1)){
+            if (newX >=t.nbLignes){newY++;}
+            if (newY >=t.nbColonnes){newX++;}
+        }
+        t.setCase(s.getX()+newX,s.getY()+newY,s);
     }
 }

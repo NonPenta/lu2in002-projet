4d2590a8301ac049ebd81f17b9202f03127e5c2b
faotix <maximilien.piron-palliser.pro@orange.fr>
2022-12-05 11:06:13 +0100

    Vent

--- classes/Vent.java
+++ classes/Vent.java
@@ -7,10 +7,10 @@ public class Vent extends Agent implements ForceEnvironnementale {
         }
         int vitesse = (int)(Math.random()*(13-1+1)+1);         // Initialisation vitesse du vent entre 130kmh et 10 kmh divisé par 10
         int newX = s.getX() + (int)(Math.random()*(vitesse-0+1)+0);       // Création du nouveau x entre 0 et la vitesse
-        int newY = s.getY() + (int)(Math.random()*((vitesse-newX)-0+1)+0);// Création du nouveau y entre 0 et newX -> Afin que le déplacement ne soit pas seul un seul axe
+        int newY = s.getY() + (vitesse-newX);// Création du nouveau y entre 0 et newX -> Afin que le déplacement ne soit pas seul un seul axe
         while(!(newX<=t.nbLignes-1) && !(newY <=t.nbColonnes-1)){
-            if (newX >=t.nbLignes){newY++;}
-            if (newY >=t.nbColonnes){newX++;}
+            if (newX >=t.nbLignes){newX=0;newY++;}
+            if (newY >=t.nbColonnes){newY=0;newX++;}
         }
         t.setCase(s.getX()+newX,s.getY()+newY,s);
     }

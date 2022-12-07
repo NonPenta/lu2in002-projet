ad8ba747e295c53cee4415b08d34f46b0d1a5efb
= <victor.j.l.bernard@gmail.com>
2022-12-08 00:41:22 +0100

    Started the task decision logic.

--- classes/Organisme.java
+++ classes/Organisme.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 public class Organisme extends Agent{
 	protected String currentTask;
-	// Tasks : locateFood, getToFood, rest, [rest_findSpot, shareFood, reproduceGatherEnergy, reproduce, locateCa, getToCa]
+	// Tasks : locateFood, getToFood, rest, [restFindSpot, shareFood, reproduceGatherEnergy, reproduce, locateCa, getToCa, moult]
 	protected HashMap<String, Double> data;
 	// type : data
-	// Collemboles : {"FoodInSystem" : double, "Energy" : double, "TSLR" : double, "Rest" : double}
-	// Isopodes : {"Ca" : double, "FoodInSystem" : double, "Energy" : double, "TSLR" : double, "Rest" : double}
-	// Serpents : {"Ca" : double, "FoodInSystem" : double, "Energy" : double, "Rest" : double}
-	// Rats : {"FoodInSystem" : double, "Energy" : double, "TSLR" : double, "Rest" : double}
+	// Collemboles : {"FoodInSystem" : double, "Energy" : double, "TSLR" : double, "Rest" : double, "Age" : double}
+	// Isopodes : {"Ca" : double, "FoodInSystem" : double, "Energy" : double, "TSLR" : double, "Rest" : double, "Age" : double}
+	// Serpents : {"Ca" : double, "FoodInSystem" : double, "Energy" : double, "Rest" : double, "Age" : double}
+	// Rats : {"FoodInSystem" : double, "Energy" : double, "TSLR" : double, "Rest" : double, "Age" : double}
+
+	// targetXLocate, targetYLocate, targetXGather, targetYGather seront ajoutés & enlevés par la fonction de choix d'action et d'action des organismes
+
 	public Organisme(String type) {
 		super(type);
 		currentTask = "rest";
@@ -40,12 +43,12 @@ public class Organisme extends Agent{
 		}
 	}
 
-	public String nextTask() {
+	public String nextTask(Terrain t, ArrayList<Organisme> ol) {
 		// Assurer la survie de l'agent ; soit répondre aux besoins en énergie & repos
 
 		// Même système pour tous les organismes, valeurs modifiées pour le serpent
 		// Économie de l'énergie & de la nourriture
-		// 0.03 d'énergie dépensée / frame si agissant ; 0.01 si se reposant
+		// 0.03 d'énergie dépensée / frame si agissant ; 0.01 si se reposant      0.003 ; 0.001 pour le serpent
 		// 1.5 d'énergie / nourriture ; obtenue a un rythme de 0.15/f si au repos ou 0.06/f si en action
 		// Taille du terrain : 20 x 20
 		// Distance moyenne depuis un côté : 20
@@ -57,19 +60,62 @@ public class Organisme extends Agent{
 		//
 		// Condition de survie : tjs > un aller-retour en réserve
 		// => Condition de recherche de nourriture : energie + nourriture * 1.5 < 9.6 * 1.5 => energie + 1.5 * nourriture < 14.4
-		// Condition d'arrêt de la recherche de nourriture : energie > 43.2 && nourriture > 28.8
+		// Condition d'arrêt de la recherche de nourriture : energie + nourriture * 1.5 > 43.2
 
 		// Repos :
-		// en action : dépense de 0.01 repos / frame
-		// au repos : gain de 0.03 repos / frame
+		// en action : dépense de 0.01 repos / frame      0.03 pour le serpent
+		// au repos : gain de 0.03 repos / frame          0.015 pour le serpent
 		// coût en repos d'un aller-retour : 320 * .01 = 3.2
 		// Condition de repos : repos < 4.8
-		// Condition d'arrêt du repos : repos > 9.6 ou autre besoin vital
-		switch (type) {
-			case "Isopode":
-			case "Collembole":
+		// Condition d'arrêt du repos : repos > 14.4
+
+		// La priorité d'un besoin vital est 1 - valeur/max
+		// Elle sert à déterminer la priorité entre deux actions urgentes
+		double potEnergy = data.get("Energy") + data.get("FoodInSystem") * 1.5;
+
+		boolean rest_u = data.get("Rest") < 4.8;
+		boolean food_u = potEnergy < 14.4;
+
+		double rest_p = 1 - data.get("Rest") / 14.4;
+		double food_p = 1 - potEnergy / 43.2;
+
+		String priority = (rest_u || food_u) ? (food_p >= rest_p ? "food" : "rest") : "other";
+
+		if (priority == "other") {
+			boolean actionDone = ((currentTask == "rest" || currentTask == "restFindSpot") && data.get("Rest") > 14.4) ||
+										((currentTask == "locateFood" || currentTask == "getToFood") && potEnergy > 43.2);
+			if (!actionDone) {
+				priority = (currentTask == "rest" || currentTask == "restFindSpot") ? "rest" : "food";
+			}
 		}
-		return "rest";
+
+		switch (priority) {
+			case "rest":
+				return canRest(t.getCase(y, x), ol) ? "rest" : "restFindSpot";
+			case "food":
+				if (!(data.containsKey("targetXGatherFood"))) {
+					return "locateFood";
+				} else {
+					return "getToFood";
+				}
+			case "other":
+				switch (type) {
+					case "Serpent":
+					case "Isopode":
+						if (data.get("Age") < 720 || type == "Serpent") { // Test redondant, mais solution plus élégante que la précédente
+							boolean canMoult = data.get("Ca") > data.get("Age") / 72;
+							return canMoult ? "moult" : (data.containsKey("targetXGatherCa") ? "getToCa" : "locateCa");
+						}
+					case "Rat":
+					case "Collembole":
+						// TODO : reproduction
+						break;
+				}
+				break;
+			default:
+				break;
+		}
+		return "";
 	}
 
 	public void act(Terrain t) {
@@ -77,5 +123,9 @@ public class Organisme extends Agent{
 			default:
 				break;
 		}
+	}
+
+	public boolean canRest(Ressource r, ArrayList<Organisme> ol) {
+		return true; // TODO
 	}
 }

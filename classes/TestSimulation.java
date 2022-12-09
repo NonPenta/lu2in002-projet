2de98da6ef1d02f593b8504a4c8dd751334b67ba
faotix <maximilien.piron-palliser.pro@orange.fr>
2022-12-09 18:13:25 +0100

    .

--- classes/TestSimulation.java
+++ classes/TestSimulation.java
@@ -25,15 +25,17 @@ public class TestSimulation {
 			u = UI.getNewUI(20,20);
 		}catch(InstantiationException e){
 			System.out.println("Fin");
-			for (int i = 0; i < 20; i++)
-				for (int j = 0; j < 20; j++)
-					u.setCell(i, j, '*',i,j);
+			for (int i = 0; i < 20*10; i++)
+				for (int j = 0; j < 255; j++)
+					u.setCell(i, j, '*',2,3);
 		}
+
 		Bundle b = new Bundle();
 		//Création des ressources dans un tableau
 		RessourceDynamique [] tabDyn = new RessourceDynamique[20];
 		RessourceStatique [] tabStat = new RessourceStatique[20];
 		RessourceTerrain [] tabTer = new RessourceTerrain[20];
+		Organisme [] tabOr = new Organisme[20];
 
 		for (int j=0;j<tabDyn.length;j++){
 			int test = (int)(Math.random() * (3-1+1)+1);
@@ -71,6 +73,21 @@ public class TestSimulation {
 			}
 		}
 
+		for (int j=0;j<tabOr.length;j++){
+			int test = (int)(Math.random() * (4-1+1)+1);
+			switch(test){
+				case 1 :
+					tabOr[j] = new Organisme("Rat");
+				case 2 :
+					tabOr[j] = new Organisme("Serpent");
+				case 3 :
+					tabOr[j] = new Organisme("Isopode");
+				case 4 :
+					tabOr[j] = new Organisme("Isopode");
+			}
+			b.addRessource(tabDyn[j].type, 1);
+		}
+
 		/*for(int j=0;j<tabDyn.length;j++){
 			int x = (int)(Math.random()* ((t.nbLignes-1)-0+1)+0);
 			int y = (int)(Math.random()* ((t.nbColonnes-1)-0+1)+0);
@@ -85,7 +102,31 @@ public class TestSimulation {
 
 		for(int i=0;i<20;i++){
 			for(int j=0;j<20;j++){
-				for (RessourceDynamique r :)
+				for (RessourceDynamique r : tabDyn){
+					switch(r.type){
+						case "Graminee":
+							u.setCell(i, j,'G');
+						case "Champignon":
+							u.setCell(i, j, 'C');
+						case "MatBio":
+						u.setCell(i, j, 'M');
+					}
+					
+				}
+			}
+		}
+
+		for(int i=0;i<20;i++){
+			for(int j=0;j<20;j++){
+				for (RessourceDynamique r : tabStat){
+					switch(r.type){
+						case "Calcium":
+							u.setCell(i, j,'A');
+						case "Moisissure":
+							u.setCell(i, j, 'O');
+					}
+					
+				}
 			}
 		}
 

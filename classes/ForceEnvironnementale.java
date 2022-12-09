619cc78dde7ad6b3669991bb85d25c0202324b21
faotix <maximilien.piron-palliser.pro@orange.fr>
2022-12-09 11:39:29 +0100

    Javadoc 1ere partie

--- classes/ForceEnvironnementale.java
+++ classes/ForceEnvironnementale.java
@@ -1,3 +1,7 @@
+/**
+ * @author BERNARD Victor
+ * @author PIRON-PALLISER Maximilien
+ */
 public interface ForceEnvironnementale{
 	public void appliquer(Terrain t, ForceEnvironnementale a);
 	public void agir(Ressource r, Terrain t);
